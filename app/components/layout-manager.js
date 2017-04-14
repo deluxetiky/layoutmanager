8f8e063b69edfdde4e00de538517bc55818b8008
Fatih Sinan Bir <f.sinanbir@gmail.com>
2017-04-14 23:15:05 +0300

    add function added.

--- app/components/layout-manager.js
+++ app/components/layout-manager.js
@@ -1,21 +1,36 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    classNameBindings: ['propertyA'],
-    propertyA: 'layout-wrapper',
-    lastSelectedElement:{},
-    didInsertElement(){
-        this._super(...arguments);
-        var mainview = $(".layout-wrapper .main-view").first();
-        console.log(mainview);
-        mainview.click((e)=> {
-       console.log('Tıklandı');      
-       this.toggleAppereance(e.target);
+  classNameBindings: ['propertyA'],
+  propertyA: 'layout-wrapper',
+  lastSelectedElement: {},
+  current: {},
+  didInsertElement() {
+    this._super(...arguments);
+    var mainview = $(".layout-wrapper .main-view").first();
+    var lastSel = $(this.get('lastSelectedElement'));
+    console.log(mainview);
+    mainview.click((e) => {
+      let target = $(e.target);
+      console.log('Tıklandı');
+     
+      this.toggleAppereance(target);      
+      console.log(target.children().find('.added-component').length);
+      this.set('lastSelectedElement', target);
     });
-},
-toggleAppereance(target){
-console.log('AppUpdate');
- console.log(target);
- $(target).toggleClass('selectedUi');
-}
+  },
+  toggleAppereance(target) {
+    console.log('AppUpdate');
+    console.log(target);
+    $(target).toggleClass('selectedUi');
+  },
+  actions: {
+    addToView() {
+      let sel = $(this.get('lastSelectedElement'));
+      let cur = this.get('current');
+      
+      console.log(sel.length);      
+      sel.append('<div class="col-sm-' + cur.size + ' added-component"><div>')
+    }
+  }
 });
