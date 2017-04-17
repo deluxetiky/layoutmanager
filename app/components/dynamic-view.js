428d5a2ec9bcb756808a898630ce217608b00e59
Fatih Sinan Bir <f.sinanbir@gmail.com>
2017-04-17 22:55:51 +0300

    Project structure updated.

--- app/components/dynamic-view.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-let layout = Ember.HTMLBars.compile(' hello');
-export default Ember.Component.extend({
-
-  attributeBindings: ['moduleName'],
-  moduleName: 'dynamicView'
-
-});
